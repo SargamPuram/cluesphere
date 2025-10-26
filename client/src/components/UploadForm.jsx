bc3fa2f0cd96f1b4a06dc6aff02d8229820060f3
SargamPuram <sargampuram3@gmail.com>
2025-10-26 22:08:01 +0530

    Added prod url

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -12,7 +12,7 @@ export default function UploadForm({ onSubmit, techniqueName }) {
   }
 
   return (
-    <div className="mt-6">
+    <div className="mt-7">
       <form onSubmit={handleSubmit} className="space-y-4">
         <div className="flex items-center justify-center w-full">
           <label className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
